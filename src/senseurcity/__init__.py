68ba149d069165a2bc2a853498cadbe71ccb1921
61825162+CaderIdris@users.noreply.github.com <61825162+CaderIdris@users.noreply.github.com>
2025-12-23 22:40:50 +0000

    2.0.0 release
    After a longer than expected development process thanks to the Influx
    detour, the SensEURCity ETL pipeline is finally set up...

--- src/senseurcity/__init__.py
+++ src/senseurcity/__init__.py
@@ -1,5 +1,9 @@
 """.. include:: ../../README.md"""  # noqa: D400, D415
 
+__version__ = "2.0.0"
+__maintainer__ = "Idris Hayward"
+__email__ = "id.hayward@caegeidwad.dev"
+
 from . import data, engine, main, orm, zipped
 
 __all__ = [
